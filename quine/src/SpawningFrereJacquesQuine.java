6b8b1052f141b2c913faf026c0d404fac3a59b63
Eric Lefevre-Ardant <contact@ericlefevre.net>
2013-02-27 21:30:36 +0100

    Frère Jacques now sung in canon!

--- quine/src/SpawningFrereJacquesQuine.java
+++ quine/src/SpawningFrereJacquesQuine.java
@@ -9,9 +9,8 @@ public class SpawningFrereJacquesQuine {
 				+ "public static void main(String[] args) throws Exception { "
 				+ "String[] paroles = {`Frere Jacques`, `Frere Jacques`, `Dormez-vous ?`, `Dormez-vous ?`, `Sonnez les matines`, `Sonnez les matines`, `Ding daing dong`, `Ding daing dong`};"
 				+ "char q = 34; String out = `[OUT]$`; String text = `[TEXT]`; "
-				+ "int index = out.lastIndexOf('$') - out.indexOf('$') ;"
-				+ "if (paroles.length > index) System.out.println(paroles[index]);"
-				+ "else System.exit(0);"
+				+ "if (out.lastIndexOf('$') - out.indexOf('$') > 4) System.exit(0);"
+				+ "System.out.println(paroles[0]);"
 				+ "PrintWriter pw = new PrintWriter(out + `.java`); "
 				+ "pw.format(text, 34, out, text); "
 				+ "pw.close(); Runtime runtime = Runtime.getRuntime(); "
@@ -19,31 +18,40 @@ public class SpawningFrereJacquesQuine {
 				+ "		Process java = runtime.exec(`java ` + out);" + 
 				"		BufferedReader stdInput = new BufferedReader(new InputStreamReader(" + 
 				"				java.getInputStream()));" + 
-				"		String s;" + 
+				"		String s;" +
+				"		int index = 0;" + 
 				"		while ((s = stdInput.readLine()) != null) {" + 
-				"			System.out.println(s);" + 
+				"			index++;" + 
+				"			if (paroles.length > index) System.out.println(paroles[index]);" + 
+				"			System.out.println(`*` + s);" + 
 				"		}" + 
-				"" +
+				"		while (paroles.length > index) {" + 
+				"			index++;" + 
+				"			System.out.println(paroles[index]);" + 
+				"		}" + 
 				"} " +
 				"}")
 				.replace("`", "%1$c").replace("[OUT]", "%2$s")
 				.replace("[TEXT]", "%3$s");
-		int index = out.lastIndexOf('$') - out.indexOf('$');
-		if (paroles.length > index)
-			System.out.println(paroles[index]);
-		else
-			System.exit(0);
+		if (out.lastIndexOf('$') - out.indexOf('$') > 4) System.exit(0);
+		System.out.println(paroles[0]);
 		PrintWriter pw = new PrintWriter(out + ".java");
 		pw.format(text, 34, out, text);
 		pw.close();
 		Runtime runtime = Runtime.getRuntime();
 		runtime.exec("javac " + out + ".java").waitFor();
 		Process java = runtime.exec("java " + out);
-		BufferedReader stdInput = new BufferedReader(new InputStreamReader(
-				java.getInputStream()));
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(java.getInputStream()));
 		String s;
+		int index = 0;
 		while ((s = stdInput.readLine()) != null) {
-			System.out.println(s);
+			index++;
+			if (paroles.length > index) System.out.println(paroles[index]);
+			System.out.println("*" + s);
+		}
+		while (paroles.length > index) {
+			index++;
+			System.out.println(paroles[index]);
 		}
 	}
 }
