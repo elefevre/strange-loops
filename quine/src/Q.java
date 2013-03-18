88a669ac54d29922d7efeab0af4bc373d48e500a
Eric Lefevre-Ardant <contact@ericlefevre.net>
2013-03-18 21:50:15 +0100

    Actual quine is a one-liner

--- quine/src/Q.java
+++ quine/src/Q.java
@@ -1,6 +1,1 @@
-class Q {
-	public static void main(String[] a) {
-		String t = "class Q { public static void main(String[] a) { String t = %c%s%c; System.out.printf(t, 34, t, 34); } }";
-		System.out.printf(t, 34, t, 34);
-	}
-}
+class Q { public static void main(String[] a) { String t = "class Q { public static void main(String[] a) { String t = %c%s%c; System.out.printf(t, 34, t, 34); } }"; System.out.printf(t, 34, t, 34); }}
