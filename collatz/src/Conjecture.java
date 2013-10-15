2423aa35c18911d4b3bcd7b35cec271988a03517
Eric Lefevre-Ardant <eric.lefevre.ext@mappy.com>
2013-10-15 10:09:10 +0200

    Cleaner code

--- collatz/src/Conjecture.java
+++ collatz/src/Conjecture.java
@@ -1,19 +1,19 @@
 public class Conjecture {
 	public static void main(String[] args) {
-		for (int i = 1; i <= 10000; i++) {
-			System.out.println(stepsTo1(0, i));
+		for (int i = 1; i <= 100; i++) {
+			System.out.print(i + "->" + stepsTo1(i) + ", ");
 		}
 	}
 
-	public static int stepsTo1(int steps, int value) {
+	public static int stepsTo1(int value) {
 		if (value == 1)
-			return steps;
+			return 0;
 
 		boolean even = value % 2 == 0;
 		if (even) {
-			return stepsTo1(steps + 1, value / 2);
+			return 1 + stepsTo1(value / 2);
 		}
-		return stepsTo1(steps + 1, (value * 3 + 1));
+		return 1 + stepsTo1(value * 3 + 1);
 	}
 
 }
